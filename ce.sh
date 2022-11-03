68d1aca5f9a6f59c4dc81754aa2f5c1b4d66413c
liguigong <liguigong@joyy.com>
2022-11-03 14:11:29 +0800

    feat: 升级版本

---
+++ ce.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PS3='请选择你要发布的版本: '
+foods=("patch" "minor" "major" "Quit")
+select fav in "${foods[@]}"; do
+    case $fav in
+        "patch")
+            echo "Americans eat roughly 100 acres of $fav each day!"
+	    # optionally call a function or run some code here
+            ;;
+        "minor")
+            echo "$fav is a Vietnamese soup that is commonly mispronounced like go, instead of duh."
+	    # optionally call a function or run some code here
+            ;;
+        "major")
+            echo "According to NationalTacoDay.com, Americans are eating 4.5 billion $fav each year."
+	    # optionally call a function or run some code here
+	    break
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+npm list
